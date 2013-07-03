eb8f3296676d4ff7c0bf955acfa7950b1e297663
japaveh <info@japaveh.nl>
2013-07-03 17:12:02 +0200

    Codesniffer fixes

--- config/general.testing.php
+++
@@ -1,7 +0,0 @@
-<?php
-return array(
-    'general' => array('style_locations' => array(
-        __DIR__ . '/../../../stylasdfasdfes/common'),
-        'image_not_found' => 'image_not_found.jpg'
-    )
-);
