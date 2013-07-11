5736ad6c82451e3539b452095b72f0cc6fd3cb74
japaveh <info@japaveh.nl>
2013-07-11 10:19:53 +0200

    Typo fix to have Travis working correctly

--- tests/config/autoload/doctrine_orm.testing.php
+++ tests/config/autoload/doctrine_orm.testing.php
@@ -6,11 +6,11 @@ return array(
             'orm_default' => array(
                 'driverClass' => 'Doctrine\DBAL\Driver\PDOMySql\Driver',
                 'params'      => array(
-                    'host'          => ((defined('DEBRANOVA_HOST') && DEBRANOVA_HOST === 'travis') ? '127.0.0.1' : 'localhost'),
+                    'host'          => (getenv('DEBRANOVA_HOST') === 'travis' ? '127.0.0.1' : 'localhost'),
                     'port'          => '3306',
-                    'user'          => ((defined('DEBRANOVA_HOST') && DEBRANOVA_HOST === 'travis') ? 'root' : 'phpci'),
-                    'password'      => ((defined('DEBRANOVA_HOST') && DEBRANOVA_HOST === 'travis') ? '' : 'AB4VF'),
-                    'dbname'        => ((defined('DEBRANOVA_HOST') && DEBRANOVA_HOST === 'travis') ? 'myapp_test' : 'test'),
+                    'user'          => (getenv('DEBRANOVA_HOST') === 'travis' ? 'root' : 'phpci'),
+                    'password'      => (getenv('DEBRANOVA_HOST') === 'travis' ? '' : 'AB4VF'),
+                    'dbname'        => (getenv('DEBRANOVA_HOST') === 'travis' ? 'myapp_test' : 'test'),
                     'driverOptions' => array(
                         PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"
                     )
