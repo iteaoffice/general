bf1d5dfe291769b3248ccbfbb4ddc19689bbb79c
japaveh <info@japaveh.nl>
2013-07-11 10:01:08 +0200

    Added suppport for the database in travis

--- tests/config/autoload/doctrine_orm.testing.php
+++ tests/config/autoload/doctrine_orm.testing.php
@@ -2,15 +2,15 @@
 
 return array(
     'doctrine' => array(
-        'connection' => array(
+        'connection'    => array(
             'orm_default' => array(
                 'driverClass' => 'Doctrine\DBAL\Driver\PDOMySql\Driver',
-                'params' => array(
-                    'host' => 'localhost',
-                    'port' => '3306',
-                    'user' => 'phpci',
-                    'password' => 'AB4VF',
-                    'dbname' => 'test',
+                'params'      => array(
+                    'host'          => ((defined('DEBRANOVA_HOST') && DEBRANOVA_HOST === 'travis') ? '127.0.0.1' : 'localhost'),
+                    'port'          => '3306',
+                    'user'          => ((defined('DEBRANOVA_HOST') && DEBRANOVA_HOST === 'travis') ? 'root' : 'phpci'),
+                    'password'      => ((defined('DEBRANOVA_HOST') && DEBRANOVA_HOST === 'travis') ? '' : 'AB4VF'),
+                    'dbname'        => ((defined('DEBRANOVA_HOST') && DEBRANOVA_HOST === 'travis') ? 'myapp_test' : 'test'),
                     'driverOptions' => array(
                         PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"
                     )
@@ -19,12 +19,12 @@ return array(
         ), // now you define the entity manager configuration
         'entitymanager' => array(
             'orm_default' => array(
-                'connection' => 'orm_default',
+                'connection'    => 'orm_default',
                 'configuration' => 'orm_default'
             ),
         ),
         //Add the dependency to the Contact namespace here for testing purposes
-        'driver' => array(
+        'driver'        => array(
             'orm_default' => array(
                 'drivers' => array(
                     'Contact\Entity' => 'general_annotation_driver'
