1f9233490c5d85dde11f04c0f958c13515177ba1
Andre Hebben <andre.hebben@sourceror.eu>
2014-09-12 07:34:29 +0000

    update

--- config/services.config.php
+++ config/services.config.php
@@ -14,6 +14,7 @@ return [
     'factories' => [
         'general_module_options' => function ($sm) {
             $config = $sm->get('Config');
+
             return new Options\ModuleOptions(isset($config['general']) ? $config['general'] : []);
         },
         EmailService::class      => function ($sm) {
