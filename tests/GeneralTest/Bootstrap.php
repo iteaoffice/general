04dec263ed8fe3ac924f80fc5d89c7616447ac99
Johan van der Heide <info@japaveh.nl>
2016-01-15 16:30:16 +0100

    Update of the tests

--- tests/GeneralTest/Bootstrap.php
+++ tests/GeneralTest/Bootstrap.php
@@ -1,5 +1,18 @@
 <?php
 
+/**
+ * ITEA Office copyright message placeholder.
+ *
+ * PHP Version 5
+ *
+ * @category    Accounting
+ *
+ * @author      Johan van der Heide <johan.van.der.heide@itea3.org>
+ * @copyright   2004-2015 ITEA Office
+ * @license     https://itea3.org/license.txt proprietary
+ *
+ * @link        https://itea3.org
+ */
 namespace GeneralTest;
 
 use RuntimeException;
@@ -10,12 +23,24 @@ use Zend\ServiceManager\ServiceManager;
 error_reporting(E_ALL | E_STRICT);
 chdir(__DIR__);
 
+
 /**
- * Test bootstrap, for setting up autoloading
+ * Test bootstrap, for setting up autoloading.
  */
 class Bootstrap
 {
+    /**
+     * @var ServiceManager
+     */
     protected static $serviceManager;
+
+    /**
+     * @return ServiceManager
+     */
+    public static function getServiceManager()
+    {
+        return static::$serviceManager;
+    }
 
     public static function init()
     {
@@ -29,82 +54,51 @@ class Bootstrap
         if (($path = static::findParentPath('src')) !== $zf2ModulePaths[0]) {
             $zf2ModulePaths[] = $path;
         }
-
         static::initAutoloader();
-        $config = include __DIR__ . '/../config/application.config.php';
+
+        // use ModuleManager to load this module and it's dependencies
+        $config = [
+            'module_listener_options' => [
+                'module_paths' => $zf2ModulePaths,
+            ],
+            'modules'                 => [
+                'General',
+            ],
+        ];
+
+
         $serviceManager = new ServiceManager(new ServiceManagerConfig());
         $serviceManager->setService('ApplicationConfig', $config);
         $serviceManager->get('ModuleManager')->loadModules();
         static::$serviceManager = $serviceManager;
-        $entityManager = $serviceManager->get('Doctrine\ORM\EntityManager');
-//        //Validate the schema;
-//        $validator = new SchemaValidator($entityManager);
-//        $errors    = $validator->validateMapping();
-//
-//        if (count($errors) > 0) {
-//            foreach ($errors AS $entity => $errors) {
-//                echo "Error in Entity: '" . $entity . "':\n";
-//                echo implode("\n", $errors);
-//                echo "\n";
-//            }
-//            die();
-//        }
-        //Create the schema
-        $tool = new \Doctrine\ORM\Tools\SchemaTool($entityManager);
-        $mdFactory = $entityManager->getMetadataFactory();
-        $mdFactory->getAllMetadata();
-        $tool->dropDatabase();
-        $tool->createSchema($mdFactory->getAllMetadata());
-//        $loader = new Loader();
-//        $loader->addFixture(new \ProjectTest\Fixture\LoadVersionData());
-//        $loader->addFixture(new \ProgramTest\Fixture\LoadDomainData());
-//        $loader->addFixture(new \GeneralTest\Fixture\LoadContentTypeData());
-//        $loader->addFixture(new \ProjectTest\Fixture\LoadProjectLogoData());
-//        $loader->addFixture(new \ProjectTest\Fixture\LoadDocumentTypeData());
-//
-//        $purger   = new ORMPurger();
-//        $executor = new ORMExecutor($entityManager, $purger);
-//        $executor->execute($loader->getFixtures());
     }
 
-    public static function getServiceManager()
+    public static function chroot()
     {
-        return static::$serviceManager;
+        $rootPath = dirname(static::findParentPath('module'));
+        chdir($rootPath);
     }
 
     protected static function initAutoloader()
     {
         $vendorPath = static::findParentPath('vendor');
-        $zf2Path = getenv('ZF2_PATH');
-        if (!$zf2Path) {
-            if (defined('ZF2_PATH')) {
-                $zf2Path = ZF2_PATH;
-            } elseif (is_dir($vendorPath . '/ZF2/library')) {
-                $zf2Path = $vendorPath . '/ZF2/library';
-            } elseif (is_dir($vendorPath . '/zendframework/zendframework/library')) {
-                $zf2Path = $vendorPath . '/zendframework/zendframework/library';
-            }
-        }
-        if (!$zf2Path) {
-            throw new RuntimeException(
-                'Unable to load ZF2. Run `php composer.phar install` or define a ZF2_PATH environment variable.'
-            );
-        }
+
         if (file_exists($vendorPath . '/autoload.php')) {
             include $vendorPath . '/autoload.php';
         }
-        include $zf2Path . '/Zend/Loader/AutoloaderFactory.php';
-        AutoloaderFactory::factory(
-            [
-                'Zend\Loader\StandardAutoloader' => [
-                    'autoregister_zf' => true,
-                    'namespaces'      => [
-                        __NAMESPACE__ => __DIR__ . '/' . __NAMESPACE__,
-                        'Admin'       => __DIR__ . '/../../../../module/Admin'
-                    ],
+
+        if (!class_exists('Zend\Loader\AutoloaderFactory')) {
+            throw new RuntimeException('Unable to load ZF2. Run `php composer.phar install`');
+        }
+
+        AutoloaderFactory::factory([
+            'Zend\Loader\StandardAutoloader' => [
+                'autoregister_zf' => true,
+                'namespaces'      => [
+                    __NAMESPACE__ => __DIR__ . '/' . __NAMESPACE__,
                 ],
-            ]
-        );
+            ],
+        ]);
     }
 
     protected static function findParentPath($path)
@@ -124,3 +118,4 @@ class Bootstrap
 }
 
 Bootstrap::init();
+Bootstrap::chroot();
