0d4064ea9c38ad2e4b343b2544ee1eb452c60848
Johan van der Heide <info@japaveh.nl>
2016-03-17 13:49:19 +0100

    Version bump

--- src/General/Version/Version.php
+++ src/General/Version/Version.php
@@ -21,7 +21,7 @@ final class Version
     /**
      * Zend Framework version identification - see compareVersion().
      */
-    const VERSION = '2.0.0';
+    const VERSION = '2.1.1-dev';
     /**
      * Github Service Identifier for version information is retrieved from.
      */
