fe78d51890e10979f25eea8d7456bb2f6e8e35b9
Johan van der Heide <info@japaveh.nl>
2017-04-03 13:27:44 +0200

    Startup of the unit test

---
+++ tests/Entity/CountryTest.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * ITEA copyright message placeholder
+ *
+ * @category    ProjectTest
+ * @package     Entity
+ * @author      Johan van der Heide <johan.van.der.heide@itea3.org>
+ * @copyright   Copyright (c) 2004-2017 ITEA Office (https://itea3.org)
+ */
+
+namespace GeneralTest\Entity;
+
+use General\Entity\Country;
+use General\Entity\EntityAbstract;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class GeneralTest
+ *
+ * @package GeneralTest\Entity
+ */
+class GeneralTest extends TestCase
+{
+    /**
+     *
+     */
+    public function testCanCreateCountry()
+    {
+        $country = new Country();
+        $this->assertInstanceOf(Country::class, $country);
+        $this->assertInstanceOf(EntityAbstract::class, $country);
+    }
+}
